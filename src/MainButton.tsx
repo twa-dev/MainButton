2713851f785af53a3c07c6c96f95e150d8f2ed50
Artur Stambultsian <bumbledos@gmail.com>
2022-08-12 23:44:07 +0400

    initial commit

---
+++ src/MainButton.tsx
@@ -0,0 +1,74 @@
+import { FC, useEffect } from "react";
+import { TelegramWindow } from "./types";
+
+export interface MainButtonProps {
+  disabled?: boolean;
+  progress?: boolean;
+  color?: string;
+  textColor?: string;
+  onClick: VoidFunction;
+  text: string;
+}
+
+const telegramWindow = window as unknown as TelegramWindow;
+
+const mainButton = telegramWindow.Telegram.WebApp.MainButton;
+const { button_color, button_text_color } =
+  telegramWindow.Telegram.WebApp.themeParams;
+
+export const MainButton: FC<MainButtonProps> = ({
+  disabled,
+  color,
+  textColor,
+  text,
+  onClick,
+  progress,
+}) => {
+  useEffect(() => {
+    mainButton.show();
+    return () => {
+      mainButton.hide();
+      mainButton.enable();
+      mainButton.hideProgress();
+      mainButton.setParams({
+        color: button_color,
+        text_color: button_text_color,
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (typeof progress === "boolean") {
+      if (progress) {
+        mainButton.showProgress();
+        mainButton.disable();
+      } else {
+        mainButton.hideProgress();
+      }
+    }
+    if (typeof disabled === "boolean") {
+      disabled || progress ? mainButton.disable() : mainButton.enable();
+    }
+  }, [disabled, progress]);
+
+  useEffect(() => {
+    if (color || textColor) {
+      mainButton.setParams({ color, text_color: textColor });
+    }
+  }, [color, textColor]);
+
+  useEffect(() => {
+    mainButton.setText(text);
+  }, [text]);
+
+  useEffect(() => {
+    if (onClick) {
+      telegramWindow.Telegram.WebApp.MainButton.onClick(onClick);
+      return () => {
+        telegramWindow.Telegram.WebApp.MainButton.offClick(onClick);
+      };
+    }
+  }, [onClick]);
+
+  return null;
+};
---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export interface MainButton {
+  isActive: boolean;
+  isVisible: boolean;
+  isProgressVisible: boolean;
+  show: VoidFunction;
+  hide: VoidFunction;
+  enable: VoidFunction;
+  disable: VoidFunction;
+  hideProgress: VoidFunction;
+  showProgress: (leaveActive?: boolean) => void;
+  onClick: (callback: VoidFunction) => void;
+  offClick: (callback: VoidFunction) => void;
+  setText: (text: string) => void;
+  setParams: (params: {
+    color?: string;
+    text?: string;
+    text_color?: string;
+    is_active?: boolean;
+    is_visible?: boolean;
+  }) => void;
+}
+
+export interface ThemeParams {
+  bg_color: string;
+  text_color: string;
+  hint_color: string;
+  link_color: string;
+  button_color: string;
+  button_text_color: string;
+  secondary_bg_color: string;
+}
+
+export interface WebApp {
+  themeParams: ThemeParams;
+  MainButton: MainButton;
+}
+
+export interface TelegramWindow extends Window {
+  Telegram: {
+    WebApp: WebApp;
+  };
+}
